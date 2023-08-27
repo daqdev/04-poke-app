d00f57fda52d1e8795e6b7ae375bceca8907f311
decu <daquiro@pm.me>
2023-08-27 13:01:54 -0300

    90c

---
+++ src/components/PokemonImage.vue
@@ -0,0 +1,17 @@
+<template>
+  <img class="hidden-pokemon" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+  <img class="fade-in" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/1.svg" alt="pokemon">
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.hidden-pokemon {
+    filter: brightness(0);
+}
+
+</style>
---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>Quien es este Pokemon</h1>
+  <pokemon-image/>
+  <pokemon-options/>
+
+  <!-- opciones -->
+</template>
+
+<script>
+import PokemonImage from '@/components/PokemonImage.vue'
+import PokemonOptions from '@/components/PokemonOptions.vue'
+export default {
+  components: { PokemonImage, PokemonOptions },
+
+}
+</script>
+
+
+
