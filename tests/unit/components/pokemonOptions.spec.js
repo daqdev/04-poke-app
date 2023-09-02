8f98d51aef1ad314ed2a20ea047d9588b72e0a21
decu <daquiro@pm.me>
2023-09-02 08:33:18 -0300

    avance 110

--- tests/unit/components/pokemonOptions.spec.js
+++ tests/unit/components/pokemonOptions.spec.js
@@ -36,6 +36,15 @@ describe('pokemon-options', ()=>{
 
     test ('debe emitir "selection" con sus parametros ', ()=>{
 
+        const [li1, li2, li3, li4]=wrapper.findAll('li')
+        li1.trigger('click')
+        li1.trigger('click')
+        li2.trigger('click')
+
+        expect(wrapper.emitted('selection').length).toBe(3)
+        
+        expect(wrapper.emitted('selection')[0]).toStrictEqual([1])
+        expect(wrapper.emitted('selection')[2]).toStrictEqual([2])
 
 
     })
